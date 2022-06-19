7c1e4a15ca00fcde752fdf99f93669920b3db2a3
Leo Bruell <leobruell@gmail.com>
2022-06-19 14:04:11 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num == 0){return ''}
+    if (num < 0){return 'ERROR'}
+    let strOut = str;
+    for (let i = 1; i < num; i++){
+        strOut += str; 
+    }
+    return strOut
 };
 
 // Do not edit below this line
