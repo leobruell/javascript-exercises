6faddfd0b57d09ba9730d6e63143fe16d14cbb27
Leo Bruell <leobruell@gmail.com>
2022-06-19 15:52:09 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arr, val) {
+        let args = [...arguments];
+        args = args.slice(1)
+        for (let i = 0; i < args.length; i++){
+            arr = arr.filter((x) => (x !== args[i]))
+        }
+        return arr
+}
+console.log(removeFromArray([1,2,3,4,5], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
