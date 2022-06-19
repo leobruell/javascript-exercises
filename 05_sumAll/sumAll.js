8b4090942302034e382ae5d1b10db10a888b6fa0
Leo Bruell <leobruell@gmail.com>
2022-06-19 16:16:05 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumTotal = 0;
+    if (typeof(num1) == 'string'|| typeof(num2) == 'string'){return 'ERROR'}
+    else if (num1 < 0 || num2 < 0){return 'ERROR'}
+    else if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+            sumTotal += i;
+        }
+        return sumTotal
+    }
+    else if (num1 > num2){
+        for (let i = num1; i >= num2; i--){
+            sumTotal += i;
+        }
+        return sumTotal 
+    }
+    else {return 'ERROR'}
 };
 
 // Do not edit below this line
